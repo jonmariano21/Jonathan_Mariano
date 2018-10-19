ebb4adcb7d0ba7c1f244d437ffe44a07dcd9822d
Jonathan <jonmariano21@gmail.com>
2018-10-18 20:01:11 -0400

    Fixed Home Page Loading Bug
    Landing page now always has 4 divs instead of randomly loading index.handlebars. Home/Back button on About Me page working.

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,16 @@ var projects = require("../projects.json")
 
 exports.view = function(req, res){
   // Here you should implement your random landing page rendering functionality
-  var random = Math.random();
-  
+  //var random = Math.random();
+
+  res.render("index_alternate", projects);
+  /*
   if( random > 0.5 ){
-	  res.render("index", projects);
+	  //res.render("index", projects);
+	  res.render("index_alternate", projects);
   }
   else{
   	res.render("index_alternate", projects);
   }
+  */
 };
